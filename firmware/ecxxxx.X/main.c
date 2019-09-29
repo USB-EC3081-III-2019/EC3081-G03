8c340404770552e73d7db40fdbb91bfb71b63bd6
José Villalobos <55517635+Jose0p3@users.noreply.github.com>
2019-09-28 22:43:40 -0400

    Se agregaron dos funciones mas
    * Se agregó la función: void LCD_Display(int Tam, int Hor) - Función que sirve para desplazar el display hacia la izquierda, con la intención de mostrar en pantalla "Str" mas largos.
    
    * Se agregó la función: int LCD_Contar(char c[]) - Función que sirve para contar la cantidad de caracteres del "Str" que se desea mostrar en pantalla, con la intención de conocer cuantos caracteres faltan por mostrar en el display, y por tanto cuantos espacios debe moverse este.

--- firmware/ecxxxx.X/main.c
+++ firmware/ecxxxx.X/main.c
@@ -6,7 +6,7 @@
 #pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
 #pragma config LVP = OFF        // Low Voltage In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
 #pragma config CPD = OFF        // Data EE Memory Code Protection (Code Protection off)
-#pragma config WRT = ON          // FLASH Program Memory Write Enable (Unprotected program memory may be written to by EECON control)
+#pragma config WRT = ON         // FLASH Program Memory Write Enable (Unprotected program memory may be written to by EECON control)
 
 # define _XTAL_FREQ 4000000
 
@@ -14,26 +14,34 @@
 #define LCD_E PORTCbits.RC0
 #define LCD_DATO PORTD
 
-
 void LCD_Escribir(char dato);
 void LCD_Comando(char dato);
 void LCD_Configuracion_Inicial();
 void PIC_Configuracion_Inicial();
 void LCD_EscribirStr(char str[]);
 void LCD_Cursor(int Horizontal, int Vertical);
+void LCD_Display(int Tam, int Hor);
+int LCD_Contar(char c[]);
+
 void main()
 {
-    int Horizontal, Vertical;
-    char aux[]="Grupo 03 AL FIN";
+    int Horizontal, Vertical, aux2;
+    char aux1[]="Grupo 03: Karlhianna - Jose";
     PIC_Configuracion_Inicial();
     LCD_Configuracion_Inicial();
-   
-    Horizontal = 4; // Desplazamientos del cursos hacia la derecha
-    Vertical = 2; // 1 Arriba, 2 Abajo
-  
-    LCD_Cursor(Horizontal, Vertical);
-    LCD_EscribirStr(aux);
-   
+    
+        aux2=LCD_Contar(aux1);
+    
+        Horizontal = 0; // Numero de cuadros del cursor que se desea desplazar hacia la derecha - 63 la primera lina, para la simulacion - 40 la primera linea para el montaje
+        Vertical = 0; // 1 Arriba, 2 Abajo
+        LCD_Cursor(Horizontal, Vertical);
+        LCD_EscribirStr(aux1);
+        
+     while(1)
+    {   
+        LCD_Display(aux2,0);
+    }
+            
     while(1){
         __delay_ms(500);
         PORTAbits.RA0 = 1;
@@ -94,7 +102,6 @@ void LCD_EscribirStr(char str[])
         LCD_Escribir(str[i]);
         i++;
     }
-   
 }
 void LCD_Cursor(int h, int v)
 {
@@ -102,21 +109,47 @@ void LCD_Cursor(int h, int v)
    
     if(v==1)
     {
-    for(i=0;i<h;i++)
-    {
-    __delay_us(40);
-    LCD_Comando(0x14);
-    }
+        for(i=0;i<h;i++)
+        {
+            __delay_us(40);
+            LCD_Comando(0x14);
+        }
     }
   
     if(v==2)
     {
         h=h+40;
-    for(i=0;i<h;i++)
-    {
-    __delay_us(40);
-    LCD_Comando(0x14);
-    }
+        for(i=0;i<h;i++)
+        {
+            __delay_us(40);
+            LCD_Comando(0x14);
+        }
     }
    
  }
+int LCD_Contar(char c[])
+{
+    int i=0;
+    while(c[i]!='\0' && i<1000)
+    {
+        i++;
+    }
+    return i;
+}
+void LCD_Display(int Tam, int Hor)
+{
+    int aux1, i;
+    __delay_ms(2);
+    LCD_Comando(0x02);
+    
+    __delay_ms(2000);
+    if(Tam>16 && Tam<40)
+    {
+        aux1 = Tam-16;
+        for(i=-1;i<aux1;i++)
+        {
+            __delay_ms(500);
+            LCD_Comando(0x1B);
+        }
+    }
+}
